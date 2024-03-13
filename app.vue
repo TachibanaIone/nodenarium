d0f1949686e776f02d601e36331d0fd5465e9856
橘いおね <tachibanaione@gmail.com>
2024-03-13 22:07:14 +0900

    OGPを追加

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script lang="ts" setup>
+useSeoMeta({
+    title: "Nodenarium",
+    description: "わたしたちはどうして、自らについての詩を書くのでしょうか？",
+    ogDescription: "わたしたちはどうして、自らについての詩を書くのでしょうか？",
+    ogImage: "https://nodenarium.net/assets/images/ogp.webp",
+})
+</script>
+
 <template>
     <NuxtLayout>
         <NuxtPage />
