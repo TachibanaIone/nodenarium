1dc3e91dae6ff42534eb79f0462099bf96d500c4
橘いおね <tachibanaione@gmail.com>
2024-09-12 05:35:25 +0900

    bump to v0.2.0

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
     <footer class="border-t-[1px] border-white">
-        <p class="text-semi mx-auto my-2 md:w-body-desktop w-body-mobile">Nodenarium v0.1.0 "Takanashi" - Developed / Maintained by <a href="//tachibanai.one" target="_blank" class="text-emerald-400 underline mx-1">橘いおね</a> with Love.</p>
+        <p class="text-semi mx-auto my-2 md:w-body-desktop w-body-mobile">Nodenarium v0.2.0 "Fujita" - Developed / Maintained by <a href="//tachibanai.one" target="_blank" class="text-emerald-400 underline mx-1">橘いおね</a> with Love.</p>
     </footer>
 </template>
